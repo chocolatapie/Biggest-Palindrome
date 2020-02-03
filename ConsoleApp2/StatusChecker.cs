e1b10962e866a1e4433cd536fbfb2cffa5a62898
Dmytro Batura <dmytro.batura@3shape.com>
2020-02-03 11:19:02 +0200

    refactoring

--- ConsoleApp2/StatusChecker.cs
+++ ConsoleApp2/StatusChecker.cs
@@ -2,10 +2,19 @@
 
 namespace ConsoleApp2
 {
+    /// <summary>
+    /// Class created for displaying messages regarding progress
+    /// </summary>
     class StatusChecker
     {
+        /// <summary>
+        /// Check whether palindrome was already found
+        /// </summary>
         public bool WasPrimeNumberFound { get; set; }
 
-        public StringBuilder PhraseForUser { get; set; }
+        /// <summary>
+        /// Contains message displaying to user regarding computing progress
+        /// </summary>
+        public StringBuilder StatusMessage { get; set; }
     }
 }
